bfcd7382f8e517f613bc7d5cea8699ab5f01e17f
Salim Jamal <jamalsalim.js12@gmail.com>
2025-05-30 09:49:46 +0000

    Cors policy added.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 const PORT = 4000;
 
